63d546136c64160eddb2277260d887c9467bbc16
Max Bigras <mbigras22@gmail.com>
2020-11-25 17:31:50 -0800

    Add features list

--- helloku.py
+++ helloku.py
@@ -7,9 +7,19 @@ from flask import Flask, jsonify
 __version__ = "0.0.1"
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
+features = [
+    "feature1",
+]
 
 
 @app.route("/", methods=["GET"])
 def up():
     logger.info("Got request to /")
-    return jsonify({"name": __name__, "version": __version__, "status": "healthy"})
+    return jsonify(
+        {
+            "name": __name__,
+            "version": __version__,
+            "status": "healthy",
+            "features": features,
+        }
+    )
